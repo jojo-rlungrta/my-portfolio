139b54f986e4e331edb3eaf2b57c99fc509a4ebe
Jonosuke Fukuda <j.fukuda@rlungrta.jp>
2025-09-16 19:36:48 +0900

    20250916

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,7 +1,7 @@
 export default function About() {
   return (
     <section className="mb-16">
-      <h2 className="text-2xl font-semibold mb-2">About Ma</h2>
+      <h2 className="text-2xl font-semibold mb-2">About Me</h2>
       <p>Front-end Developer and Web Director with over 8 years of experience designing and managing corporate websites across diverse sectors.<br />Proficient in HTML, CSS, JavaScript, and jQuery, with a strong focus on user-centered development.</p>
       <p>Currently expanding expertise in React, Next.js, and TypeScript through self-directed learning to stay aligned with modern frontend practices.</p>
       <p>Preparing to relocate to Canada on a Working Holiday visa in October 2025.<br />Actively seeking short-term or contract-based frontend development opportunities in Toronto or remote settings.</p>
