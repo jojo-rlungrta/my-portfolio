9354ab5712f3cf796e6ee6ab5de8245f62c53444
Jonosuke Fukuda <j.fukuda@rlungrta.jp>
2025-11-29 13:13:35 -0500

    20251129

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,7 +5,6 @@ export default function About() {
       <p className="mt-6">Front-end Developer and Web Project Manager with 8+ years of experience delivering high-quality corporate websites for enterprise clients in Japan.</p>
       <p>I specialize in performance-optimized, maintainable, and user-centered frontend development, <br />focusing on accessibility, dynamic content handling, and smooth CMS integration.</p>
       <p>Currently expanding expertise in React, Next.js, and TypeScript to align with modern frontend practices.</p>
-      <p>Based in Canada on a Working Holiday Visa, seeking short-term or contract frontend opportunities to contribute high-quality, maintainable web solutions to local teams.</p>
     </section>
   );
 }
